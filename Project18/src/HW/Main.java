2c6718dd8f8d49b1b442cdb56fb7221d1493fbc8
Vlad1234514092005 <terebilo.vlad1409@gmail.com>
2023-09-27 17:14:49 +0200

    Added Main.java

--- Project18/src/HW/Main.java
+++ Project18/src/HW/Main.java
@@ -11,14 +11,37 @@ public class Main {
     public static void getVowelsAndConsonants(String string, char[] vowels, char[] consonants) {
         int consonantsNum = 0;
         int vowelsNum = 0;
-        for(char vowel : vowels)
-            for(char letter : string.toLowerCase().toCharArray())
-                if(vowel == letter) vowelsNum++;
-        for(char consonant : consonants)
-            for(char letter : string.toLowerCase().toCharArray())
-                if(consonant == letter) consonantsNum++;
+        for (char vowel : vowels)
+            for (char letter : string.toLowerCase().toCharArray())
+                if (vowel == letter) vowelsNum++;
+        for (char consonant : consonants)
+            for (char letter : string.toLowerCase().toCharArray())
+                if (consonant == letter) consonantsNum++;
         System.out.println("Согласных: " + consonantsNum);
         System.out.println("Гласных: " + vowelsNum);
+    }
+
+
+    public static String removeAllSpaces(String string) {
+        int counter = 0;
+        char[] letterMassive = string.toCharArray();
+        for (int i = 0; i < string.length(); i++) {
+            if (letterMassive[i] == ' ') {
+                counter++;
+            }
+        }
+        char[] newLetterMassive = new char[string.length() - counter];
+        int currentLetterCounter = 0;
+        for(int i = 0; i < string.length(); i++){
+            if(letterMassive[i] != ' '){
+                newLetterMassive[currentLetterCounter] = letterMassive[i];
+                currentLetterCounter++;
+//                currentLetterCounter += 5;
+//                currentLetterCounter = currentLetterCounter + 5;
+//                currentLetterCounter + 5;
+            }
+        }
+        return new String(newLetterMassive);
     }
 
     public static void main(String[] args) {
@@ -30,8 +53,7 @@ public class Main {
         char[] consonantsRus = {'б', 'в', 'г', 'д', 'ж', 'x', 'з', 'й', 'к', 'л', 'м', 'н', 'п', 'р', 'с', 'т', 'ф', 'ц', 'ч', 'щ', 'ш'};
         char[] vowelsEng = {'a', 'e', 'o', 'i', 'u', 'y'};
         char[] consonantsEng = {'q', 'w', 'r', 't', 'p', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm'};
-        getVowelsAndConsonants(inputString, vowelsRus, consonantsRus);
-        inputString = inputString.replaceAll(" ", "");
-        System.out.println(inputString);
+        getVowelsAndConsonants(inputString, vowelsEng, consonantsEng);
+        System.out.println(removeAllSpaces(inputString));
     }
 }
