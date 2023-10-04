610beebb0d4f6b95cdc5657f508426c44a9dcbe0
Vlad1234514092005 <terebilo.vlad1409@gmail.com>
2023-10-04 17:32:54 +0200

    Added Main.java

--- Project22/src/hw/Main.java
+++ Project22/src/hw/Main.java
@@ -3,46 +3,77 @@ package hw;
 import java.util.*;
 
 public class Main {
-    public static void theLongestString(List<String> stringList) {
+    public static void findTheLongestString(List<String> stringList) {
         int maxLength = 0;
-        String theLongestStr = "";
-        for (int i = 0; i < stringList.size(); i++) {
-            if (stringList.get(i).length() > maxLength) {
-                maxLength = stringList.get(i).length();
-                theLongestStr = stringList.get(i);
+        String theLongestString = "";
+        for (int index = 0; index < stringList.size(); index++) {
+            String currentString = stringList.get(index);
+            if (currentString.length() > maxLength) {
+                maxLength = currentString.length();
+                theLongestString = currentString;
             }
         }
-        System.out.println("Самая длинная строка: " + theLongestStr);
+        System.out.println("Самая длинная строка: " + theLongestString);
         System.out.println("Ее длина : " + maxLength);
     }
 
     public static int countPositiveNumbers(List<Integer> numberArray) {
         int sum = 0;
-        for (int i = 0; i < numberArray.size(); i++) {
-            if (numberArray.get(i) > 0) {
-                sum += numberArray.get(i);
+        for (int index = 0; index < numberArray.size(); index++) {
+            if (numberArray.get(index) > 0) {
+                sum += numberArray.get(index);
             }
         }
         return sum;
     }
 
     public static void searchEqualElementsInSet(Set<Integer> set1, Set<Integer> set2) {
-        List<Integer> equalSetElements = new ArrayList<>();
-        for(int i: set1){
-            for(int j: set2){
-                if(i == j){
-                    equalSetElements.add(i);
+        Set<Integer> equalSetElements = findEqualElementsInSet(set1, set2);
+        Iterator<Integer> iterator = equalSetElements.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+        for (Integer putinChujlo : equalSetElements) {
+            System.out.print(putinChujlo + " ");
+        }
+        System.out.println();
+    }
+
+    private static Set<Integer> findEqualElementsInSet(Set<Integer> setBig, Set<Integer> setSmall) {
+        Set<Integer> equalSetElements = new HashSet<>();
+        for (int firstSetElem : setBig) {
+            for (int secondSetElem : setSmall) {
+                if (firstSetElem == secondSetElem) {
+                    equalSetElements.add(firstSetElem);
                 }
             }
         }
-        for(int i = 0; i < equalSetElements.size(); i++){
-            System.out.print(i + " ");
-        }
+        return equalSetElements;
     }
 
-    public static boolean isSubSet(Set<Integer> set1, Set<Integer> set2){
-        if(set1.size() < set2.size()){
+    public static boolean isSubSet(Set<Integer> set1, Set<Integer> set2) {
+        Set<Integer> totalSet = new HashSet<>();
+        totalSet.addAll(set1);
+        totalSet.addAll(set2);
+        if (set1.size() == set2.size()) {
+            if (totalSet.size() == set1.size()) {
+                return true;
+            } else {
+                return false;
+            }
 
+        } else if (set1.size() > set2.size()) {
+            if (totalSet.size() == set1.size()) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            if (totalSet.size() == set2.size()) {
+                return true;
+            } else {
+                return false;
+            }
         }
     }
 
@@ -52,7 +83,7 @@ public class Main {
         stringArray.add("cat");
         stringArray.add("dogs");
         stringArray.add("elephant");
-        theLongestString(stringArray);
+        findTheLongestString(stringArray);
         List<Integer> numberArray = new ArrayList<>();
         numberArray.add(5);
         numberArray.add(9);
@@ -61,19 +92,11 @@ public class Main {
         numberArray.add(-6);
         numberArray.add(-16);
         System.out.println(countPositiveNumbers(numberArray));
-        Set<Integer> numSet1 = new HashSet<>();
-        Set<Integer> numSet2 = new HashSet<>();
-        numSet1.add(3);
-        numSet1.add(38);
-        numSet1.add(4);
-        numSet1.add(13);
 
-        numSet2.add(6);
-        numSet2.add(45);
-        numSet2.add(14);
-        numSet2.add(13);
-        numSet2.add(5);
-
-//        searchEqualElementsInSet(numSet1, numSet2);
+        searchEqualElementsInSet(Set.of(2, 5, 7, 11, 8), Set.of(4, 8, 2, 1, 7, 12, 9));
+        System.out.println(isSubSet(Set.of(2, 5, 7, 11, 8), Set.of(4, 8, 2, 1, 7, 12, 9)));
+        System.out.println(isSubSet(Set.of(2, 5, 7, 11, 8), Set.of(2, 5)));
+        System.out.println(isSubSet(Set.of(2, 5), Set.of(2, 5, 7, 11, 8)));
+        System.out.println(isSubSet(Set.of(2, 5), Set.of(2, 5)));
     }
 }
