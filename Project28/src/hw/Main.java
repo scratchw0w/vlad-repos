7ac276dd7882c0d433d163792c22d2709c2b2f66
Vlad1234514092005 <terebilo.vlad1409@gmail.com>
2023-10-21 10:58:51 +0200

    Added Main.java

--- Project28/src/hw/Main.java
+++ Project28/src/hw/Main.java
@@ -3,94 +3,87 @@ package hw;
 import java.util.*;
 
 public class Main {
-    public static void countNumberOfWordsInText(HashMap<String, Integer> yourMap, String[] wordsArray) {
-        int counter = 0;
-        for (int i = 0; i < wordsArray.length; i++) {
-            for (int j = 0; j < wordsArray.length; j++) {
-                if (wordsArray[i].equals(wordsArray[j])) {
-                    counter += 1;
+    private static final Scanner SCANNER = new Scanner(System.in);
+
+    private static Map<String, Integer> countNumberOfWordsInText(String[] wordsArray) {
+        int wordsFrequency = 0;
+        Map<String, Integer> wordsWithFrequency = new HashMap<>();
+
+        for (int outsideIndex = 0; outsideIndex < wordsArray.length; outsideIndex++) {
+            for (int innerIndex = 0; innerIndex < wordsArray.length; innerIndex++) {
+                if (wordsArray[outsideIndex].equals(wordsArray[innerIndex])) {
+                    wordsFrequency += 1;
                 }
             }
-            yourMap.put(wordsArray[i], counter);
-            counter = 0;
+            wordsWithFrequency.put(wordsArray[outsideIndex], wordsFrequency);
+
+            wordsFrequency = 0;
         }
+
+        return wordsWithFrequency;
     }
 
-    public static boolean addNewContact(HashMap<String, String> contactBook) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Input name --> ");
-        String name = scanner.nextLine();
+    private static void addNewContact(Map<String, String> contactBook) {
+        System.out.println("Input name for adding contact --> ");
+        String name = SCANNER.nextLine();
         System.out.println("Input phone number --> ");
-        String phoneNumber = scanner.nextLine();
-        for (int i = 0; i < contactBook.size(); i++) {
-            if (contactBook.get(i) == name) {
-                return false;
-            }
-        }
-        contactBook.put(name, phoneNumber);
-        return true;
-    }
-
-    public static boolean removeContact(HashMap<String, String> contactBook) {
-        Scanner scanner = new Scanner(System.in);
-        int counter = 0;
-        System.out.println("Input name --> ");
-        String name = scanner.nextLine();
-        for (int i = 0; i < contactBook.size(); i++) {
-            if (contactBook.get(i) == (name)) {
-                counter += 1;
-            }
-        }
-        if (counter != 0) {
-            return false;
-        }
-        contactBook.remove(name);
-        return true;
-    }
-
-    public static void searchContactByName(HashMap<String, String> contactBook) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Input name --> ");
-        String name = scanner.nextLine();
-        for (int i = 0; i < contactBook.size(); i++) {
-            if (contactBook.get(i) == name) {
-                System.out.println(name);
+        String phoneNumber = SCANNER.nextLine();
+        for (String currentName : contactBook.keySet()) {
+            if (currentName.equals(name)) {
                 return;
             }
         }
+        contactBook.put(name, phoneNumber);
     }
 
-    public static void addSetElemsToList(HashSet<Integer> set, List<Integer> list) {
-        for (Integer current : set) {
-            list.add(current);
-        }
+    private static void removeContact(Map<String, String> contactBook) {
+        System.out.println("Input name for removing contact --> ");
+        String name = SCANNER.nextLine();
+        contactBook.remove(name);
     }
 
-    public static void saveEqualElements(HashSet<Integer> set, List<Integer> list) {
-        int counter = 0;
-        for (int i = 0; i < list.size(); i++) {
-            for (int j = 0; j < list.size(); j++) {
-                if (list.get(i) == list.get(j)) {
-                    counter+=1;
+    private static void searchContactByName(Map<String, String> contactBook) {
+        System.out.println("Input name for searching contact --> ");
+        String name = SCANNER.nextLine();
+        System.out.println(contactBook.get(name));
+//        for (String currentName : contactBook.keySet()) {
+//            if (currentName.equals(name)) {
+//                System.out.println(name);
+//                return;
+//            }
+//        }
+    }
+
+    private static void saveEqualElements(Set<Integer> set, List<Integer> list) {
+        int elementEqualCounter = 0;
+        for (int outsideIndex = 0; outsideIndex < list.size(); outsideIndex++) {
+            Integer currentElement = list.get(outsideIndex);
+            for (int innerIndex = 0; innerIndex < list.size(); innerIndex++) {
+                if (currentElement.equals(list.get(innerIndex))) {
+                    elementEqualCounter += 1;
                 }
             }
-            if(counter > 1){
-                set.add(list.get(i));
+
+            if (isRepeatableElement(elementEqualCounter)) {
+                set.add(currentElement);
             }
-            counter = 0;
+
+            elementEqualCounter = 0;
         }
+    }
+
+    private static boolean isRepeatableElement(int counter) {
+        return counter > 1;
     }
 
     public static void main(String[] args) {
-        //Task 2
-        String text = "London is the capital of Great Britain";
-        String[] words = text.split(" ");
-        HashMap<String, Integer> wordsInTextAndItsFrequency = new HashMap<>();
-        countNumberOfWordsInText(wordsInTextAndItsFrequency, words);
-        System.out.println(wordsInTextAndItsFrequency);
-
+//        //Task 2
+//        String text = "London is the capital of of britain Great Britain".toLowerCase();
+//        String[] words = text.split(" ");
+//        System.out.println(countNumberOfWordsInText(words));
+//
         //Task 3
-        HashMap<String, String> contactBook = new HashMap<>();
+        Map<String, String> contactBook = new HashMap<>();
         addNewContact(contactBook);
         addNewContact(contactBook);
         addNewContact(contactBook);
@@ -99,29 +92,28 @@ public class Main {
         System.out.println(contactBook);
         searchContactByName(contactBook);
 
-        //Task 1
-        HashSet<Integer> set1 = new HashSet<>();
-        HashSet<Integer> set2 = new HashSet<>();
-        set1.add(0);
-        set1.add(2);
-        set1.add(5);
-        set1.add(3);
-        set1.add(7);
-        set1.add(8);
-        set1.add(10);
-
-        set2.add(2);
-        set2.add(10);
-        set2.add(12);
-        set2.add(7);
-        set2.add(6);
-
-        List<Integer> twoSetsList = new ArrayList<>();
-        addSetElemsToList(set1, twoSetsList);
-        addSetElemsToList(set2, twoSetsList);
-        System.out.println(twoSetsList);
-        HashSet<Integer> totalSet = new HashSet<>();
-        saveEqualElements(totalSet, twoSetsList);
-        System.out.println(totalSet);
+//        //Task 1
+//        Set<Integer> set1 = new HashSet<>();
+//        Set<Integer> set2 = new HashSet<>();
+//        set1.add(0);
+//        set1.add(2);
+//        set1.add(5);
+//        set1.add(3);
+//        set1.add(7);
+//        set1.add(8);
+//        set1.add(10);
+//
+//        set2.add(2);
+//        set2.add(10);
+//        set2.add(12);
+//        set2.add(7);
+//        set2.add(6);
+//
+//        List<Integer> twoSetsList = new ArrayList<>(set1);
+//        twoSetsList.addAll(set2);
+//        System.out.println(twoSetsList);
+//        Set<Integer> totalSet = new HashSet<>();
+//        saveEqualElements(totalSet, twoSetsList);
+//        System.out.println(totalSet);
     }
 }
